0622547c4cf30d788e9944f7db3c665b300beae2
TranPham <42188469+trancoder@users.noreply.github.com>
2024-03-19 16:32:11 -0500

    Update Mystruct.cpp

--- Mystruct.cpp
+++ Mystruct.cpp
@@ -1,13 +1,5 @@
 struct MyStruct {
     int id;
     double value;
-    std::string name;
-
-    // Boost Serialization for the struct
-    template<class Archive>
-    void serialize(Archive & ar, const unsigned int version) {
-        ar & id;
-        ar & value;
-        ar & name;
-    }
+    char name[20];
 };
